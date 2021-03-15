86d6e7bf07a7fc95cac0bc55602705acda8452bc
Shin-Hyunjin <astimegoesbyshj@naver.com>
2021-03-15 12:57:49 +0900

    added menumanager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,12 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Income");
+		System.out.println("2. Savings");
+		System.out.println("3. Expenses");
+		System.out.println("4. Budget Summary");
+		System.out.println("5. Exit");
+	}
+
+}
