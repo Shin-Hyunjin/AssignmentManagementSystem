b0de48aaf1b4f8a9d604cadd4fe0526a1f0bb764
Shin-Hyunjin <astimegoesbyshj@naver.com>
2021-04-06 12:10:29 +0900

    Modified

--- src/Assignment.java
+++ src/Assignment.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Assignment {
 		String subject = "no assignment";
@@ -24,5 +25,28 @@ public class Assignment {
 			this.professor = professor;
 			this.assignName = assignName;
 			this.deadline = deadline;	
+		}
+			
+		public void ViewMenu() {
+			System.out.println("");
+			System.out.println("*** Assignment Management System Menu ***");
+			System.out.println("1. Add Assignments");
+			System.out.println("2. Submitted Assignments");
+			System.out.println("3. Edit Assignments");
+			System.out.println("4. View Assignments");
+			System.out.println("5. Exit");
+			System.out.print("Select one number between 1 - 5 : ");
+			}
+		
+		public void printinfoEdit(){
+			System.out.println("");
+			System.out.println("--- Edit Assignments ---");
+			System.out.println("--- What kind of information do you want to edit? ---");
+			System.out.println("1. Edit Subject Name");
+			System.out.println("2. Edit Professor Name");
+			System.out.println("3. Edit Assignment Name");
+			System.out.println("4. Edit Deadline");
+			System.out.println("5. Edit Assignment Score");
+			System.out.print("Select one number between 1 - 5 : ");
 		}
 }
--- src/MenuManager.java
+++ src/MenuManager.java
@@ -3,22 +3,15 @@
 	public class MenuManager {
 		
 		public static void main(String[] args) {
-			int menuNum = 5;
+			int menuNum = 0;
 			String [] assignInfo = {"no assignment","no assignment","no assignment","0"};
 			double score = 0.0;
 			Scanner input = new Scanner(System.in);
 			AssignmentManager assignmentManager = new AssignmentManager(input); 
-					
-			while(menuNum != 6) {
-				System.out.println("");
-				System.out.println("*** Assignment Management System Menu ***");
-				System.out.println("1. Add Assignments");
-				System.out.println("2. Submitted Assignments");
-				System.out.println("3. Edit Assignments");
-				System.out.println("4. View Assignments");
-				System.out.println("5. Show a menu");
-				System.out.println("6. Exit");
-				System.out.print("Select one number between 1 - 6 : ");
+			Assignment assignment = new Assignment();
+			
+			while(menuNum != 5) {
+				assignment.ViewMenu();
 				menuNum = input.nextInt();
 	            
 				if(menuNum == 1) {
@@ -39,19 +32,10 @@
 					}
 					else break;
 					
-					
 				}
 
 				else if(menuNum == 3) {
-					System.out.println("");
-					System.out.println("--- Edit Assignments ---");
-					System.out.println("--- What kind of information do you want to edit? ---");
-					System.out.println("1. Edit Subject Name");
-					System.out.println("2. Edit Professor Name");
-					System.out.println("3. Edit Assignment Name");
-					System.out.println("4. Edit Deadline");
-					System.out.println("5. Edit Assignment Score");
-					System.out.print("Select one number between 1 - 5 : ");
+					assignment.printinfoEdit();
 					int editNum = input.nextInt();
 
 					if(editNum != 5) {
@@ -75,10 +59,12 @@
 						continue;
 					}
 					else break;	
-					
-				}
-				else; 
+					 }
+				
+				else break;
+				
 			}
-		}   
+		}
 	}
+
 
